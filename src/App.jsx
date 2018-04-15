a3b7bfbf79302c5c92064e5d68716aa7c8749e59
Kedar Naik <knaik@fhlbdm.com>
2018-04-14 22:06:25 -0500

    Added a basic alert to show error from react bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import Clock from './Clock';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { Form, FormControl, Button, Alert } from 'react-bootstrap';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
+        this.handleDismiss = this.handleDismiss.bind(this);
+        this.handleShow = this.handleShow.bind(this);
         this.state = {
             deadline: 'December 16, 2018',
-            newDeadLine: ''
+            newDeadLine: '',
+            show: false
         }
     }
 
     changeDeadline() {
         if (this.state.newDeadLine) {
             this.setState({ deadline: this.state.newDeadLine });
+        } else {
+            this.handleShow();
         }
+
     }
 
+    handleDismiss() {
+        this.setState({ show: false });
+      }
+    
+      handleShow() {
+        this.setState({ show: true });
+      }
+
     render() {
+        if (this.state.show) {
+            return (
+              <Alert bsStyle="danger" onDismiss={this.handleDismiss}>
+                <h4>Oh snap! You got an error!</h4>
+                <p>
+                  Please enter the date in the format that is given in the heading.
+                </p>
+                <p>
+                  <Button onClick={this.handleDismiss}>Hide Alert</Button>
+                </p>
+              </Alert>
+            );
+          }
+
+
         return (
             <div className="App">
                 <div className="App-title">Countdown to {this.state.deadline}</div>
